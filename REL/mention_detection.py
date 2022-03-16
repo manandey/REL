4c8ed717abbf5243d7ad90340f765553c913fd6d
Manan Dey <manandey1@gmail.com>
2022-03-16 17:01:22 +0530

    set `mini_batch_size=8`

--- REL/mention_detection.py
+++ REL/mention_detection.py
@@ -122,7 +122,7 @@ class MentionDetection(MentionDetectionBase):
         results = {}
         total_ment = 0
         if is_flair:
-            tagger.predict(processed_sentences)
+            tagger.predict(processed_sentences, mini_batch_size=8)
         for i, doc in enumerate(dataset_sentences_raw):
             contents = dataset_sentences_raw[doc]
             raw_text = dataset[doc][0]
